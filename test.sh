3307f8c1e70e07fe6d9cf749d17d93593115bbba
quapka <quapka@gmail.com>
2021-06-16 22:22:24 +0200

    Fix broken tests after scripts renaming

--- test.sh
+++ test.sh
@@ -7,5 +7,4 @@ test -f masterlist-dsc.pem || sh  gen-csca-dsc.sh
 
 # JSON / CBOR / COSE / ZLIB / Base45
 #
-echo '{ "A" : "B" }' | ${PYTHON} cose_sign.py |  ${PYTHON} cose_verify.py
-
+echo '{ "A" : "B" }' | ${PYTHON} hc1_sign.py |  ${PYTHON} hc1_verify.py
